cc4e09ce1b9db50cba0491e36425f6296bdf14e1
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-07 02:57:49 +0000

    added transpose function to wordsearch and another test to test_worsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+// Transposes an array
+const transpose = function (matrix) {
+    // Put your solution here
+    let newArray = [];
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        let col = [];
+
+        for (let j = 0; j < matrix.length; j++) {
+            col.push(matrix[j][i]);
+            //console.log("Column", col);
+        }
+        newArray.push(col);
+        // console.log("newArray", newArray)
+    }
+    return newArray;
+};
+
+
+const wordSearch = (letters, word) => {
+    //Joins the row elements together
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    
+    //Tranposes the array and joins the row elements together
+    const vertArray = transpose(letters).map(x => x.join(''));
+
+    // Checks the horizontal array for a match
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    // Checks the vertical array for a match
+    for (v of vertArray) {
+        if (v.includes(word)) return true
+    }
+
+    // Returns false if nothing is found
+    return false;
 }
 
 module.exports = wordSearch
